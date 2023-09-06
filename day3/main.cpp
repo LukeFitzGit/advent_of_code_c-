6ba8c4fa624cbe77214349d7d9fbb3b35804cbdb
Luke Fitzgerald <luke.fitzgerald.ie@gmail.com>
2023-09-07 00:20:24 +0100

    optimize & day 3 part 2

--- day3/main.cpp
+++ day3/main.cpp
@@ -2,15 +2,36 @@
 #include<string>
 #include<vector>
 #include<fstream>
+#include<algorithm>
+#include<unordered_set>
 
-char GetCommonCharacter(std::string firstHalf, std::string secHalf){
-     for (char c : firstHalf){
-        for (char c2 : secHalf){
-            if (c == c2){
+char GetCommonCharacter(std::string& firstPart, std::string& secPart, std::string thirdPart = ""){
+    std::unordered_set<char> firstString(firstPart.begin(), firstPart.end());
+    std::unordered_set<char> secondString(secPart.begin(), secPart.end());
+    if (thirdPart != ""){
+        std::unordered_set<char> thirdString(thirdPart.begin(), thirdPart.end());
+        for (char c : firstString) {
+            if (secondString.count(c) && thirdString.count(c)) {
                 return c;
             }
         }
-     }
+    }
+    else{
+        for (char c : firstString) {
+            if (secondString.count(c)) {
+                return c;
+            }
+        }
+    }
+}
+
+int GetNumberFromCharacter(char character, int charOffsetForUpper, int charOffsetForLower){
+        if (isupper(character)){
+            return character + charOffsetForUpper;
+        }
+        else{
+            return character + charOffsetForLower;
+        }
 }
 
 int main(){
@@ -25,22 +46,26 @@ int main(){
         lines.close();
     }
 
-    std::string firstHalf;
-    std::string secHalf;
+    std::string* firstHalf = new std::string;
+    std::string* secHalf = new std::string;
     char value;
     int total = 0;
     for (std::string line : linesVec){
-        firstHalf = line.substr(0, line.size()/2);
-        secHalf = line.substr(line.size()/2, line.size()-1);
-        value = GetCommonCharacter(firstHalf, secHalf);
-        if (isupper(value)){
-            total += value - 38;
-        }
-        else{
-            total += value - 96;
-        }
+        *firstHalf = line.substr(0, line.size()/2);
+        *secHalf = line.substr(line.size()/2, line.size()-1);
+        value = GetCommonCharacter(*firstHalf, *secHalf);
+        total += GetNumberFromCharacter(value, -38, -96);
     }
+    delete firstHalf;
+    delete secHalf;
     std::cout << total << std::endl;
 
+    total = 0;
+    for (int i = 0; i < linesVec.size(); i+=3){
+        value = GetCommonCharacter(linesVec[i], linesVec[i+1], linesVec[i+2]);
+        total += GetNumberFromCharacter(value, -38, -96);
+    }
+    std::cout << total << std::endl;
+    
     return 0;
 }
